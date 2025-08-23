014adc141df6ecc93b1a9f9d3f425c21ccc49e48
coreywaldrep1-maker <coreywaldrep1@gmail.com>
2025-08-23 17:17:19 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 // app/layout.jsx
 import './globals.css';
+import SiteHeader from '../components/SiteHeader';
+import HomeMenu from '../components/HomeMenu';
 import SiteFooter from '../components/SiteFooter';
 
 export const metadata = {
@@ -11,6 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <SiteHeader />
+        {/* Floating menu appears on every page, left side */}
+        <HomeMenu />
         {children}
         <SiteFooter />
       </body>
