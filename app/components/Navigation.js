ed13295754a7627089f6fc0b602abb0192f3ddd7
coreywaldrep1-maker <coreywaldrep1@gmail.com>
2025-09-06 20:29:11 -0500

    Add Navigation component with AuthButton

---
+++ app/components/Navigation.js
@@ -0,0 +1,10 @@
+import AuthButton from './AuthButton';
+
+export default function Navigation() {
+  return (
+    <nav>
+      {/* Other navigation items */}
+      <AuthButton />
+    </nav>
+  );
+}
