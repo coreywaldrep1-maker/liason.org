b00402dc4fd856de66143845fb0f4dc7ac501ec2
coreywaldrep1-maker <coreywaldrep1@gmail.com>
2025-10-03 01:42:45 +0000

    Remove stray app/_not-found route; add server-safe not-found page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,13 +1,9 @@
-// app/not-found.jsx
 export default function NotFound() {
   return (
-    <main className="mx-auto max-w-3xl p-8">
-      <h1 className="text-3xl font-bold">Page not found</h1>
-      <p className="mt-2 text-gray-600">Sorry, we couldn’t find that page.</p>
-
-      <a href="/" className="inline-block mt-6 underline">
-        Go back home
-      </a>
-    </main>
+    <div style={{padding: '3rem', textAlign: 'center'}}>
+      <h1>Page not found</h1>
+      <p>Sorry, we couldn’t find that page.</p>
+      <a href="/">Go home</a>
+    </div>
   );
 }
