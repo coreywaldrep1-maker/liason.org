170f7cedefb7425feff986fa2bd6c8209744c092
coreywaldrep1-maker <coreywaldrep1@gmail.com>
2025-10-02 20:53:37 -0500

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,9 +1,14 @@
+// app/not-found.jsx
 export default function NotFound() {
   return (
-    <div style={{padding: '3rem', textAlign: 'center'}}>
-      <h1>Page not found</h1>
-      <p>Sorry, we couldn’t find that page.</p>
-      <a href="/">Go home</a>
-    </div>
+    <main style={{ maxWidth: 720, margin: '4rem auto', padding: '0 1rem' }}>
+      <h1 style={{ fontSize: 28, fontWeight: 600 }}>Page not found</h1>
+      <p style={{ marginTop: 12 }}>
+        Sorry, we couldn’t find that page.
+      </p>
+      <a href="/" style={{ marginTop: 24, display: 'inline-block', textDecoration: 'underline' }}>
+        Go home
+      </a>
+    </main>
   );
 }
