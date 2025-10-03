60cf947f4e554c1bab1f9b7a0fbfcc68ca94542a
coreywaldrep1-maker <coreywaldrep1@gmail.com>
2025-10-02 21:05:08 -0500

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,14 +1,6 @@
-// app/not-found.jsx
+// app/not-found.jsx   <-- no "use client" here
+import NotFoundClient from '../components/not-found-client'; // direct import, no barrel
+
 export default function NotFound() {
-  return (
-    <main style={{ maxWidth: 720, margin: '4rem auto', padding: '0 1rem' }}>
-      <h1 style={{ fontSize: 28, fontWeight: 600 }}>Page not found</h1>
-      <p style={{ marginTop: 12 }}>
-        Sorry, we couldn’t find that page.
-      </p>
-      <a href="/" style={{ marginTop: 24, display: 'inline-block', textDecoration: 'underline' }}>
-        Go home
-      </a>
-    </main>
-  );
+  return <NotFoundClient />;
 }
