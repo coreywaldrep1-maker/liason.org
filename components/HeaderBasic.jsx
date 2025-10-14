c302d0f649ed99583077e8224f65497dccd33e42
coreywaldrep1-maker <coreywaldrep1@gmail.com>
2025-10-13 21:00:30 -0500

    Update HeaderBasic.jsx

--- components/HeaderBasic.jsx
+++ components/HeaderBasic.jsx
@@ -27,9 +27,7 @@ export default function HeaderBasic() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-white">
       <div className="mx-auto max-w-screen-2xl px-4">
-        {/* Make this relative so the center brand can be absolutely centered */}
         <div className="relative flex h-14 items-center justify-between gap-3">
-
           {/* LEFT: hamburger + language */}
           <div className="flex items-center gap-2">
             <button
@@ -45,13 +43,20 @@ export default function HeaderBasic() {
             <LanguageSwitcher />
           </div>
 
-          {/* CENTER: logo + brand (truly centered regardless of left/right widths) */}
+          {/* CENTER: logo + brand (hard size clamp) */}
           <Link
             href="/"
             className="absolute left-1/2 -translate-x-1/2 flex min-w-0 items-center gap-2"
             aria-label="Go to homepage"
           >
-            <img src="/logo.svg" alt="Liason logo" className="h-7 w-auto shrink-0" />
+            <img
+              src="/logo.svg"
+              alt="Liason logo"
+              width={112}
+              height={28}
+              className="shrink-0"
+              style={{ height: 28, width: 'auto', maxWidth: 160, display: 'block' }}
+            />
             <span className="truncate font-semibold tracking-tight">Liason</span>
           </Link>
 
